4f386a1738992f168a23400b56940c988a763db8
justin <justin@justinmorgan.io>
2024-04-03 20:22:16 +0000

    create custom.d.ts

---
+++ custom.d.ts
@@ -0,0 +1,18 @@
+type responseItemType = {
+
+    id: string;
+    name: string;
+
+};
+
+type WeatherDetailType = {
+
+    zipcode: string;
+    weather: string;
+    temp?: number;
+
+};
+
+interface WeatherQueryInterface {
+    zipcode: string;
+}
